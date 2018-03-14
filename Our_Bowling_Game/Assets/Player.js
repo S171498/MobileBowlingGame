efa0b6d784d480c154a676cad95b5968fa9f2eca
Shane Stephenson <s171498@ucs.ac.uk>
2018-03-14 12:13:31 +0000

    Mouse movement in, camera follows.

---
+++ Our_Bowling_Game/Assets/Player.js
@@ -0,0 +1,36 @@
+﻿#pragma strict
+
+								//JP's line of code (9,10, 13, 30-36)
+								//AM's line of code (5-7, 14, 17-27)
+public var speed: float;
+public var thrust: float;
+private var rb: Rigidbody;
+
+var pointScore: int=0;
+var scoreLabel : UI.Text;
+
+function Start () {
+	pointScore = 0;
+	rb= GetComponent.<Rigidbody>();
+}
+
+function Update () {
+scoreLabel.text = pointScore.ToString("Score: " + pointScore + "/5");			
+}
+
+function FixedUpdate () {
+
+var moveHorizontal : float = Input.GetAxis ("Horizontal");
+var moveVertical : float = Input.GetAxis ("Vertical");
+
+var movement = Vector3(moveHorizontal, 0.0f, moveVertical);
+rb.AddForce (movement * speed);									
+}
+
+function OnTriggerEnter (other:Collider){											
+if (other.tag == "Collectible")														
+{																					
+pointScore++;																		
+Destroy(other.gameObject);															
+}
+}
