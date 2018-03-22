e34af70de6826a9d013327a418e6efa267e11a0a
Shane Stephenson <s171498@ucs.ac.uk>
2018-03-22 14:42:04 +0000

    First Playable

--- Our_Bowling_Game/Assets/Player.js
+++ Our_Bowling_Game/Assets/Player.js
@@ -2,20 +2,19 @@
 
 								//JP's line of code (9,10, 13, 30-36)
 								//AM's line of code (5-7, 14, 17-27)
-public var speed: float;
-public var thrust: float;
-private var rb: Rigidbody;
+var speed: float;
+var thrust: float;
+var rb: Rigidbody;
 
-var pointScore: int=0;
-var scoreLabel : UI.Text;
 
-function Start () {
-	pointScore = 0;
+
+function Start() 
+{
 	rb= GetComponent.<Rigidbody>();
 }
 
 function Update () {
-scoreLabel.text = pointScore.ToString("Score: " + pointScore + "/5");			
+			
 }
 
 function FixedUpdate () {
@@ -27,10 +26,3 @@ var movement = Vector3(moveHorizontal, 0.0f, moveVertical);
 rb.AddForce (movement * speed);									
 }
 
-function OnTriggerEnter (other:Collider){											
-if (other.tag == "Collectible")														
-{																					
-pointScore++;																		
-Destroy(other.gameObject);															
-}
-}
