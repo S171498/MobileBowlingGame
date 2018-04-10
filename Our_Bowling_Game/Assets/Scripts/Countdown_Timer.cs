ffdd41fc1a98df6c39e54b4662f391a0cab5f153
mr-mulligan <aaron95mulligan@gmail.com>
2018-04-10 16:22:39 +0100

    Work
    -Added water
    -Removed unneeded files
    -Changed lighting

---
+++ Our_Bowling_Game/Assets/Scripts/Countdown_Timer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Countdown_Timer : MonoBehaviour
+{
+    public float timeReset;
+    public float timeLeft;
+    public Text countdownText;
+
+    private void Awake()
+    {
+        timeLeft = timeReset;
+    }
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+        countdownText.text = "" + timeLeft.ToString("F0");
+
+    }
+}
