f4f2553e483c5c06c3bcf567f01a7ce240d2ce3e
Shane Stephenson <s171498@ucs.ac.uk>
2018-04-26 10:06:58 +0100

    Little boost when the player hits the stop box to give them speed to reach the end of the lane

---
+++ Crazy Bowling V2/Assets/Scripts/MaintainSpeed.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaintainSpeed : MonoBehaviour {
+
+    public int constantSpeed = 5;
+    private Rigidbody rb;
+
+    // Use this for initialization
+    void Start () {
+
+        rb = GetComponent<Rigidbody>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Stop")
+        {
+            rb.velocity = constantSpeed * (rb.velocity.normalized); 
+        }
+    }
+}
