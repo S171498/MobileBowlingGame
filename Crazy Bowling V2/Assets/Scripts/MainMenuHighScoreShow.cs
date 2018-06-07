af5e61176e4a0c6bb73124f5dd3504dbaee94601
S171498 <S171498@ucs.ac.uk>
2018-06-07 10:58:16 +0100

    Web build made. Errors on menu fixed.

---
+++ Crazy Bowling V2/Assets/Scripts/MainMenuHighScoreShow.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuHighScoreShow : MonoBehaviour {
+
+    public Text highScoreHolder;
+    public float Score;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        highScoreHolder.text = PlayerPrefs.GetFloat("HighScore", 0).ToString();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Score > PlayerPrefs.GetFloat("HighScore", 0))
+        {
+            PlayerPrefs.SetFloat("HighScore", Score);
+            highScoreHolder.text = "" + Score.ToString("N0");
+        }
+
+    }
+}
